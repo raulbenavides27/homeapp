4d1f9181861d4f911cd56269deae971e89dce385
raulbenavides27 <ra.benavides@duocuc.cl>
2023-03-21 13:31:45 -0300

    ACT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'login',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
