e71b6c925017820c1c566fb2498f53294c2d3eab
raulbenavides27 <ra.benavides@duocuc.cl>
2023-03-27 10:32:41 -0300

    Problemas con git

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup,FormControl,Validators } from '@angular/forms';
+import { AlertController, NavController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  formularioLogin: FormGroup;
 
-  constructor() { }
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrol:NavController){
+    
+    this.formularioLogin = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'pass': new FormControl("",Validators.required)
+    })
+   }
+  ngOnInit(){
+  }
 
-  ngOnInit() {
+
+  async ingresar(){
+    var f = this.formularioLogin.value;
+
+    var usuario = JSON.parse(localStorage.getItem('usuario')!);
+
+    if(usuario.nombre == f.nombre && usuario.password == f.pass){
+      console.log('Ingresado');
+
+      localStorage.setItem('ingresado','true');
+      this.navCtrol.navigateRoot('menu/inicio');
+    }else{
+      const alert = await this.alertController.create({
+        header: 'Datos incorrectos',
+        message: 'Los datos que ingresaste son incorrectos.',
+        buttons: ['Aceptar']
+      });
+  
+      await alert.present();
+    }
   }
 
 }
