189b7733317d0f057067f887175898865cd4f8e3
raulbenavides27 <ra.benavides@duocuc.cl>
2023-03-28 17:44:25 -0300

    guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { IngresadoGuard } from './ingresado.guard';
+import { NoIngresadoGuard } from './no-ingresado.guard';
 
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate: [NoIngresadoGuard]
   },
   {
     path: '',
@@ -13,24 +16,25 @@ const routes: Routes = [
   },
   {
     path: 'bienvenida',
-    loadChildren: () => import('./bienvenida/bienvenida.module').then( m => m.BienvenidaPageModule)
+    loadChildren: () => import('./bienvenida/bienvenida.module').then( m => m.BienvenidaPageModule),
+   canActivate: [IngresadoGuard]
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule),
+    canActivate: [NoIngresadoGuard]
   },
   {
     path: 'registro',
-    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule),
+    canActivate: [NoIngresadoGuard]
   },
   {
     path: 'addpropiedad',
-    loadChildren: () => import('./addpropiedad/addpropiedad.module').then( m => m.AddpropiedadPageModule)
+    loadChildren: () => import('./addpropiedad/addpropiedad.module').then( m => m.AddpropiedadPageModule),
+    canActivate: [IngresadoGuard]
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+  
 ];
 
 @NgModule({
