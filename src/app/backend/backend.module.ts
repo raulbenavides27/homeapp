be99b3bb3eb96d2c4bb9dd2eb069fddf927f38a4
raulbenavides27 <ra.benavides@duocuc.cl>
2023-04-09 12:47:44 -0400

    captura por entradas

--- src/app/backend/backend.module.ts
+++ src/app/backend/backend.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SetPropiedadComponent } from './set-propiedad/set-propiedad.component';
 import { IonicModule } from '@ionic/angular';
-
+import { FormsModule, } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +11,10 @@ import { IonicModule } from '@ionic/angular';
   ],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FormsModule,
+   
+    
   ]
 })
 export class BackendModule { }
--- src/app/backend/set-propiedad/set-propiedad.component.ts
+++ src/app/backend/set-propiedad/set-propiedad.component.ts
@@ -1,30 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Directive, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { FirestoreService } from 'src/app/services/firestore.service';
-
+import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-set-propiedad',
   templateUrl: './set-propiedad.component.html',
   styleUrls: ['./set-propiedad.component.scss'],
 })
 export class SetPropiedadComponent  implements OnInit {
+  nombre!: string;
+  direccion!: string;
 
   constructor(public menucontroler: MenuController,
-              public FirestoService: FirestoreService) { }
-
-  ngOnInit() {}
-openMenu(){
+              public FirestoService: FirestoreService) {
+               }
+              
+  ngOnInit(){}
+  openMenu(){
   console.log('open menu');
   this.menucontroler.toggle('principal');
 }
 guardarPropiedad() 
 { 
-  const data = {
-    nombre: 'prueba',
-    direccion: 'cruz'
-  };
+const data=
+ {
+  nombre: this.nombre,
+  direccion:  this.direccion
+};
 const path ='Propiedad/';
-const id = '00001';
-this.FirestoService.creatDoc(data,path,id)
+const id = '00002';
+this.FirestoService.creatDoc(data,path,id);
 }
 }
