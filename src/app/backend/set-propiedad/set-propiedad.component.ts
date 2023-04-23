e0632b27693607503709debb74ff88a7c1a61c05
raulbenavides27 <ra.benavides@duocuc.cl>
2023-04-23 19:28:33 -0400

    correciones y otros

--- src/app/backend/set-propiedad/set-propiedad.component.ts
+++ src/app/backend/set-propiedad/set-propiedad.component.ts
@@ -1,7 +1,8 @@
-import { Component, Directive, OnInit } from '@angular/core';
+import { Component, Directive, OnInit, importProvidersFrom } from '@angular/core';
 import { AlertController, LoadingController, MenuController, ToastController } from '@ionic/angular';
 import { FirestoreService } from 'src/app/services/firestore.service';
 import { Propiedad } from 'src/app/models';
+import {FirestorageService} from 'src/app/services/firestorage.service'
 @Component({
   selector: 'app-set-propiedad',
   templateUrl: './set-propiedad.component.html',
@@ -11,15 +12,17 @@ export class SetPropiedadComponent  implements OnInit {
   Propiedades: Propiedad[] = []
   newPropiedad!: Propiedad;
   enableNewPropiedad = false;
-
-private path = 'Propiedad/';
-loading: any;
+  enablelista = true;
+  private path = 'Propiedad/';
+  newImage = '';
+  loading: any;
 
   constructor(public menucontroler: MenuController,
               public FirestoService: FirestoreService,
               public loadingController:LoadingController,
               public toastController:ToastController,
-              public alertController: AlertController) { }
+              public alertController: AlertController,
+              public firestorageService: FirestorageService) { }
               
   ngOnInit(){
     this.getPropiedad();
@@ -51,7 +54,7 @@ async deletePropiedad(P: Propiedad){
   const alert = await this.alertController.create({
     cssClass: '',
     header: 'Advertencia',
-    message: 'Seguro desea <strong>eliminar</strong> esta propiedad',
+    message: '¿Seguro desea <strong>eliminar</strong> esta propiedad?',
     buttons:[{
       text: 'Cancelar',
       role: 'Cancel',
@@ -79,6 +82,7 @@ await alert.present();
 
 bntNuevo(){
   this.enableNewPropiedad = true;
+  this.enablelista = false;
   this.newPropiedad = {
     nombre: '',
     direccion: '',
@@ -106,5 +110,21 @@ async presentToast(msg: string){
     duration: 2000
   });
   toast.present();
+}
+async nuevaImagen(event:any){
+//  console.log(event);
+//if (event.target.files && event.target.files[0]){
+//    const reader = new FileReader();
+//     reader.onload = ((image: any)=>{
+//     this.newImage = image.target.result as string;
+//     });
+ //    reader.readAsDataURL(event.target.files[0]);
+//}
+const path = 'Propiedad';
+const name = 'prueba';
+const file = event.target.files[0];
+const res = await this.firestorageService.uploadImage(file,path,name);
+console.log('recibi res de la promesa ', res);
+
 }
 }
--- src/app/services/firestorage.service.ts
+++ src/app/services/firestorage.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { resolve } from 'dns';
 @Injectable({
   providedIn: 'root'
 })
 export class FirestorageService {
 
   constructor(public FireStorage: AngularFireStorage ) { }
+
+  uploadImage(file: any, path:string, nombre:string): Promise<string>{
+    return new Promise( resolve => {
+
+      const filePath = path + '/' + nombre;
+      const ref = this.FireStorage.ref(filePath);
+      const task = ref.put(file);
+      resolve('este es el enlace');
+    
+
+    });
+  }
 }
