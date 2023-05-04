1b78338954a5ea120ca6ceeb51ae0595d4b5035b
raulbenavides27 <ra.benavides@duocuc.cl>
2023-05-03 23:29:42 -0400

    aact

--- src/app/services/firebaseauth.service.ts
+++ src/app/services/firebaseauth.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseauthService {
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth) { }
+  
+  login(email:string, password:string){
+   return  this.auth.signInWithEmailAndPassword(email,password);
+  }
+  logout(){
+    this.auth.signOut();
+  }
+  registrar(email:string, password:string){
+   return this.auth.createUserWithEmailAndPassword(email,password);
+  }
 }
