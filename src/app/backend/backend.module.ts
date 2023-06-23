112fa6528b57f9b8279ab4a42f33620847ccce9b
raulbenavides27 <ra.benavides@duocuc.cl>
2023-06-23 16:23:54 -0400

    union de ramas

--- src/app/backend/backend.module.ts
+++ src/app/backend/backend.module.ts
@@ -3,11 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SetPropiedadComponent } from './set-propiedad/set-propiedad.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, } from '@angular/forms';
-<<<<<<< HEAD
-=======
 import { CuentasComponent } from './cuentas/cuentas.component';
 import { EstadoComponent } from './estado/estado.component';
->>>>>>> origin/aye
 
 @NgModule({
   declarations: [
