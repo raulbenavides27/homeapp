eb275f8bb1e3ca7215e0412ea442516fad7bbd09
raulbenavides27 <ra.benavides@duocuc.cl>
2023-06-26 18:59:30 -0400

    act

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { FirestoreService } from 'src/app/services/firestore.service';
+import { Propiedad } from 'src/app/models';
 
 @Component({
   selector: 'app-home',
@@ -9,25 +10,22 @@ import { FirestoreService } from 'src/app/services/firestore.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent  implements OnInit {
- private path = 'Propiedades/'
-
+  private path = 'Propiedad/'
+  propiedad: Propiedad[] = [];
   constructor(public menucontroler: MenuController,
     public firestoreService: FirestoreService,
               private router:Router) { 
-
                 this.loadProductos();
               }
-
   ngOnInit() {}
-
   openMenu(){
-
     console.log('open menu');
     this.menucontroler.toggle('principal');
   }
   loadProductos(){
-    this.firestoreService.getColletion(this.path).subscribe( res => {
+    this.firestoreService.getColletion<Propiedad>(this.path).subscribe( res => {
     console.log(res);
+    this.propiedad = res;
     });
   }
 
