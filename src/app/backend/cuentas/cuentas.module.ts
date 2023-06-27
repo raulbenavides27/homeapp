1134d44f155d1647beb9779752df020745af86cb
raulbenavides27 <ra.benavides@duocuc.cl>
2023-06-27 16:05:29 -0400

    act

--- src/app/backend/cuentas/cuentas.module.ts
+++ src/app/backend/cuentas/cuentas.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { MenuPageRoutingModule } from './menu-routing.module';
-import { MenuPage } from './menu.page';
+
+
 
 @NgModule({
+  declarations: [],
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    MenuPageRoutingModule
-  ],
-  declarations: [MenuPage]
+    FormsModule,
+  ]
 })
-export class MenuPageModule {}
+export class CuentasModule { }
