3c3c1051b038cba252cb04b4b70e48c8d9bfcc70
ayelencorrotea <101226466+ayelencorrotea@users.noreply.github.com>
2023-07-09 18:32:12 -0400

    searchbar funcional

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,38 +9,44 @@ import { Propiedad } from 'src/app/models';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent  implements OnInit {
-  private path = 'Propiedad/'
-  propiedad: Propiedad[] = [];
-  constructor(public menucontroler: MenuController,
+export class HomeComponent implements OnInit {
+  private path = 'Propiedad/';
+  propiedades: Propiedad[] = [];
+  filteredProperties: Propiedad[] = [];
+  searchTerm: string = '';
+  selectedSegment: string = 'Propiedad';
+
+  constructor(
+    public menucontroler: MenuController,
     public firestoreService: FirestoreService,
-              private router:Router) { 
-                this.loadProductos();
-              }
+    private router: Router
+  ) {
+    this.loadProductos();
+  }
+
   ngOnInit() {}
-  openMenu(){
+
+  openMenu() {
     console.log('open menu');
     this.menucontroler.toggle('principal');
   }
-  loadProductos(){
-    this.firestoreService.getColletion<Propiedad>(this.path).subscribe( res => {
-    console.log(res);
-    this.propiedad = res;
+
+  loadProductos() {
+    this.firestoreService.getColletion<Propiedad>(this.path).subscribe((res) => {
+      console.log(res);
+      this.propiedades = res;
+      this.filteredProperties = res; // Initialize the filtered properties array
     });
   }
 
-/*
-
-  gocuentas (){ 
-    this.router.navigate(['cuentas']);
+  filterProperties() {
+    if (this.searchTerm.trim() !== '') {
+      this.filteredProperties = this.propiedades.filter((prop) => {
+        const propValues = Object.values(prop).join(' ').toLowerCase();
+        return propValues.includes(this.searchTerm.toLowerCase());
+      });
+    } else {
+      this.filteredProperties = this.propiedades;
+    }
   }
-  goestado (){ 
-    this.router.navigate(['estado']);
-  }  
-  
-  gopropiedades (){ 
-    this.router.navigate(['set-propiedad']);
-  }
-*/  
-
 }
