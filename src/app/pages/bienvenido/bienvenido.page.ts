7041ecc66d19b643285ce41ef8a86af4ca90f7ba
raulbenavides27 <ra.benavides@duocuc.cl>
2023-07-17 13:46:25 -0400

    act

--- src/app/pages/bienvenido/bienvenido.page.ts
+++ src/app/pages/bienvenido/bienvenido.page.ts
@@ -1,5 +1,9 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { Cliente } from 'src/app/models';
+import { FirebaseauthService } from 'src/app/services/firebaseauth.service';
 
 @Component({
   selector: 'app-bienvenido',
@@ -7,13 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bienvenido.page.scss'],
 })
 export class BienvenidoPage implements OnInit {
-
-  constructor(private route: Router) { }
+  
+  
+  constructor(private route: Router,
+    public firebaseauthService: FirebaseauthService,) { }
 
   ngOnInit() {
   }
   goToLogin() {
-    this.route.navigate(['/perfil']);
-}
 
+    this.firebaseauthService.stateAuth().subscribe( res =>{
+      console.log(res);
+      if (res !== null){
+        this.route.navigate(['/home']);
+      } else {
+        this.route.navigate(['/perfil']);
+      }
+     })
+    
+}
+ 
 }
